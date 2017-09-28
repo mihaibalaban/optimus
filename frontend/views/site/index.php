6f6d865f0114ba147e0305d567a2a9e0ca5db576
mihai19cm <mihai.balaban94@yahoo.com>
2017-09-29 02:16:04 +0300

    first commit

---
+++ frontend/views/site/index.php
@@ -0,0 +1,98 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="container">
+        <section>
+            <div class="image" data-type="background" data-speed="2"></div>
+            <div class="stuff" data-type="content">
+                <div>
+                    <h1 style="font-size: 55px;">Succesul tau</h1>
+                    <h1 style="font-size: 55px;">Misiunea noastra</h1>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="image" data-type="background" data-speed="7"></div>
+            <div class="stuff" data-type="content">
+
+                <div class="row center-cards">
+                    <div class="col-lg-6">
+                        <div class="card firstCard">
+
+                            <!--Card image-->
+                            <img class="img-fluid fitIn" src="images/otsolutions.jpg" alt="Card image cap">
+                            <!--Card content-->
+                            <div class="card-body">
+                                <!--Title-->
+                                <h4 class="card-title">Card title</h4>
+                                <!--Text-->
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Button</a>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <div class="card wtransnetCard">
+
+                            <!--Card image-->
+                            <img class="img-fluid fitIn" src="images/wtransnet.jpg" alt="Card image cap">
+
+                            <!--Card content-->
+                            <div class="card-body">
+                                <!--Title-->
+                                <h4 class="card-title">Card title</h4>
+                                <!--Text-->
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Button</a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+        <section>
+            <div class="image" data-type="background" data-speed="6"></div>
+            <div class="stuff" data-type="content">Accusamus at ex amet perferendis atque fuga, ad earum, ipsa
+                perspiciatis, dolore neque, accusantium consectetur quis voluptatibus culpa architecto animi natus
+                facere expedita sed. Voluptatem odit explicabo amet neque aut.
+            </div>
+        </section>
+
+        <section>
+            <div class="image" data-type="background" data-speed="5"></div>
+            <div class="stuff" data-type="content">Accusamus at ex amet perferendis atque fuga, ad earum, ipsa
+                perspiciatis, dolore neque, accusantium consectetur quis voluptatibus culpa architecto animi natus
+                facere expedita sed. Voluptatem odit explicabo amet neque aut.
+            </div>
+        </section>
+
+        <section>
+            <div class="image" data-type="background" data-speed="3"></div>
+            <div class="stuff" data-type="content">Accusamus at ex amet perferendis atque fuga, ad earum, ipsa
+                perspiciatis, dolore neque, accusantium consectetur quis voluptatibus culpa architecto animi natus
+                facere expedita sed. Voluptatem odit explicabo amet neque aut.
+            </div>
+        </section>
+
+        <section>
+            <div class="image" data-type="background" data-speed="3"></div>
+            <div class="stuff" data-type="content">Accusamus at ex amet perferendis atque fuga, ad earum, ipsa
+                perspiciatis, dolore neque, accusantium consectetur quis voluptatibus culpa architecto animi natus
+                facere expedita sed. Voluptatem odit explicabo amet neque aut.
+            </div>
+        </section>
+    </div>
+
+    <div class="at-twitter"><a href="//twitter.com/hendrysadrak" target="_blank">@hendrysadrak</a></div>
+</div>
