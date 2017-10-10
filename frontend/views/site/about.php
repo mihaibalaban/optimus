14c97a97fdaddf5cb169584cbbc52e1dd67a37ed
mihai19cm <mihai.balaban94@yahoo.com>
2017-10-11 01:21:36 +0300

    about us

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -4,13 +4,215 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+<?php
 
-    <code><?= __FILE__ ?></code>
+/* @var $this yii\web\View */
+
+use kartik\tabs\TabsX;
+use yii\bootstrap\Button;
+
+$this->title = 'Optimus: despre noi';
+?>
+<div class="site-index">
+    <div class="container">
+        <section class="firstSection">
+            <div class="image aboutUs">
+            </div>
+            <div class="stuff" data-type="content">
+                <p class="aboutUsTextHeader">
+                    Lorem ipsum dolor sit amet, ex persius democritum persequeris mei. Has cu impetus docendi
+                    referrentur. No vel nullam cetero. Suas dictas ius ea, option fuisset his cu, cu has enim mutat. Id
+                    cetero phaedrum qualisque pri, elitr libris nonumes quo ex.
+
+                    Mei cu iriure ancillae, libris delectus mei eu. Propriae voluptua ei cum, aperiam abhorreant
+                    scripserit ex mel. Est ne tota lucilius inciderint, ei vis amet ipsum evertitur. Audiam prompta
+                    sensibus in per, has adhuc perfecto aliquando ei. Munere percipitur nec et, et inermis admodum sit.
+                    Qui et autem complectitur, eu docendi blandit definiebas quo. Sit id sumo vide, quod vivendo ut quo.
+                </p>
+            </div>
+        </section>
+        <div class="contentPadding">
+            <div class="paddingBigElements">
+                <div class="ACon">
+                    <div class="Profile">
+                        <div class="Img">
+                            <div class="overlay">
+                                <span><img/></span>
+                            </div>
+                        </div>
+                        <div class="clickPopUp">
+                            <h4><a class="username">Alexandra Offenberg</a></h4>
+                            <!--                            <h5 class="buttons"><a class="username" href="">Profile</a></h5>-->
+                            <h5 class="buttons"><a class="username">Durduca firmei</a></h5>
+                            <!--                            <h5 class="buttons"><a class="username" href="">Portfolio</a></h5>-->
+                            <!--                            <h5 class="buttons b"><a class="username" href="">Favorites</a></h5>-->
+                            <!--                            <div class="Social">-->
+                            <!--                                <svg viewBox="0 0 512 512" id="logo" class="codepen">-->
+                            <!--                                    <path class="box"-->
+                            <!--                                          d="M427 201.9c-0.6-4.2-2.9-8-6.4-10.3L264.2 87.3c-4.9-3.3-11.4-3.3-16.3 0L91.4 191.6c-4 2.7-6.5 7.4-6.5 12.2v104.3c0 4.8 2.5 9.6 6.5 12.2l156.4 104.3c4.9 3.3 11.4 3.3 16.3 0L420.6 320.4c4-2.6 6.6-7.4 6.6-12.2V203.9C427.1 203.2 427.1 202.6 427 201.9 427 201.7 427.1 202.6 427 201.9zM270.7 127.1l115.2 76.8 -51.5 34.4 -63.8-42.7V127.1zM241.3 127.1v68.6l-63.8 42.7 -51.5-34.4L241.3 127.1zM114.3 231.4l36.8 24.6 -36.8 24.6V231.4zM241.3 384.9L126.1 308.1l51.5-34.4 63.8 42.6V384.9zM256 290.8l-52-34.8 52-34.8 52 34.8L256 290.8zM270.7 384.9V316.3l63.8-42.6 51.5 34.4L270.7 384.9zM397.7 280.6l-36.8-24.6 36.8-24.6V280.6z"/>-->
+                            <!--                                </svg>-->
+                            <!--                                <svg viewBox="0 0 512 512" id="logo">-->
+                            <!--                                    <path class="box"-->
+                            <!--                                          d="M211.9 197.4h-36.7v59.9h36.7V433.1h70.5V256.5h49.2l5.2-59.1h-54.4c0 0 0-22.1 0-33.7 0-13.9 2.8-19.5 16.3-19.5 10.9 0 38.2 0 38.2 0V82.9c0 0-40.2 0-48.8 0 -52.5 0-76.1 23.1-76.1 67.3C211.9 188.8 211.9 197.4 211.9 197.4z"/>-->
+                            <!--                                </svg>-->
+                            <!--                                <svg viewBox="0 0 512 512" id="logo">-->
+                            <!--                                    <path d="M422.6 193.6c-5.3-45.3-23.3-51.6-59-54 -50.8-3.5-164.3-3.5-215.1 0 -35.7 2.4-53.7 8.7-59 54 -4 33.6-4 91.1 0 124.8 5.3 45.3 23.3 51.6 59 54 50.9 3.5 164.3 3.5 215.1 0 35.7-2.4 53.7-8.7 59-54C426.6 284.8 426.6 227.3 422.6 193.6zM222.2 303.4v-94.6l90.7 47.3L222.2 303.4z"/>-->
+                            <!--                                </svg>-->
+                            <!--                            </div>-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="customSectionAboutUs paddingBigElements">
+                <div class="image" data-type="background" data-speed="6">
+                    <div class="stufff" data-type="content">
+
+                        <div class="row" style="height: 100%;">
+                            <div class="col-lg-8 col-md-8 third-content-a">
+                                <div class="row wc">
+                                    <div class="col-lg-12">
+                                        <img class="img-fluid" src="images/logo_w_connecta.gif" alt="Card image cap">
+                                    </div>
+                                    <div class="col-lg-8" style="    margin-top: 18px;">
+
+                                        Oportunitatea perfecta de a interactiona cu alte companii pentru a va putea
+                                        prezenta
+                                        serviciile si a identifica parteneri pentru cursele dumneavoastra. Reprezinta
+                                        unul
+                                        dintre
+                                        cele mai importante evenimente din domeniul transporturilor si este organizat
+                                        anual
+                                        de
+                                        catre
+                                        Bursa Wtransnet.
+                                        <a href="#" class="btn btn-primary moreButton" style="margin-top: 14%;">Afla mai
+                                            multe</a>
+                                    </div>
+
+                                    <div class="col-lg-4">
+
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-4 third-content-b">
+                                <img class="wconecta-image" src="images/aboutUs-valori.jpg">
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="customSectionAboutUs paddingBigElements">
+                <div class="image" data-type="background" data-speed="6">
+                    <div class="stufff" data-type="content">
+
+                        <div class="row" style="height: 100%;">
+                            <div class="col-lg-6 col-md-6">
+                                <img src="images/aboutUs-mission.jpg">
+                            </div>
+                            <div class="col-lg-6 col-md-6">
+                                <h3>MISIUNEA NOASTRA</h3>
+                                <p>Este de a pune la dispozitia sectorului de transport un portofoliu de servicii de
+                                    cea mai inalta calitate ce sa acopere atat nevoile companiilor de transport cat si
+                                    caselor de expeditii.
+                                </p>
+                                <p>In momentul de fata peste 70% din clientii care ajung sa utilizeze unul dintre
+                                    serviciile puse la dispozitie de Optimus Transport provin din recomandari. Acest
+                                    lucru este foarte important pentru noi si confirma aprecierea calitatii serviciilor
+                                    oferite.
+                                </p>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
 </div>
+
+<script>
+    // Hide Header on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 6;
+    var navbarHeight = $('.hideMenu').outerHeight();
+
+    $(window).scroll(function (event) {
+        didScroll = true;
+    });
+
+    setInterval(function () {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight) {
+            // Scroll Down
+            $('.hideMenu').removeClass('nav-down').addClass('nav-up');
+        } else {
+            // Scroll Up
+            if (st + $(window).height() < $(document).height()) {
+                $('.hideMenu').removeClass('nav-up').addClass('nav-down');
+            }
+        }
+
+        lastScrollTop = st;
+    }
+
+    var expandID = [1, 2, 3, 4, 5, 6, 7];
+
+    $("#author_bio_wrap_toggleAnglia").click(function () {
+        $("#author_bio_wrap").slideToggle("fast");
+
+    });
+
+    $(document).ready(function () {
+        var $Menu = $('.Img');
+        $('.Img').mouseenter(function () {
+            $('.PopUp').css('opacity', '1');
+            $('.PopUp').css('margin-top', '20px');
+        });
+        $('.Img').mouseleave(function () {
+            $('.PopUp').css('opacity', '0');
+            $('.PopUp').css('margin-top', '0px');
+        });
+        $('.Img').on('click', function () {
+            if ($Menu.hasClass('Img')) {
+                $('.Img').addClass('click');
+                $('.Img').removeClass('Img');
+                $('.Profile').addClass('clickProfile');
+                $('.Profile').removeClass('Profile');
+                $('.clickPopUp').css('display', 'block');
+                $('.PopUp').css('display', 'none');
+            } else {
+                $('.click').addClass('Img');
+                $('.click').removeClass('click');
+                $('.clickProfile').addClass('Profile');
+                $('.clickProfile').removeClass('clickProfile');
+                $('.clickPopUp').css('display', 'none');
+                $('.PopUp').css('display', 'block');
+            }
+        });
+    });
+
+</script>
